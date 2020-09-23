91067979968344d9d1956da1f95bb8f0891c1ec3
alanpacaud <alan.pacaud@laposte.net>
2020-09-23 21:24:41 +0200

    up scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,7 +20,7 @@
     $('a.smoth-scroll').on('click', function (e) {
       var anchor = $(this);
       $('html, body').stop().animate({
-        scrollTop: $(anchor.attr('href')).offset().top - 300
+        scrollTop: $(anchor.attr('href')).offset().top - 200
       }, 1000);
       e.preventDefault();
     });
